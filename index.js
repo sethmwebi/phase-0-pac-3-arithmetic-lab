72e05d1151cb44771830b2c688badf1d5dc02c70
Seth Mwebi <sethmwebi27@gmail.com>
2024-06-21 13:54:34 +0300

    writes functions on javascript calculator challenger in index.js file

---
+++ index.js
@@ -0,0 +1,31 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const divide = (a, b) => {
+  return a / b;
+};
+
+const increment = (a) => {
+  return ++a;
+};
+
+const decrement = (a) => {
+  return --a;
+};
+
+const makeInt = (a) => {
+  return parseInt(a, 10);
+};
+
+const preserveDecimal = (a) => {
+  return parseFloat(a);
+};
